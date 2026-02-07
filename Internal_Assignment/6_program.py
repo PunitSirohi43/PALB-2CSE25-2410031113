1b246e4f33af58b56c326a085905d4c15fcc96f3
PunitSirohi43 <punit.sirohi.cs28@iilm.edu>
2026-02-07 10:28:44 +0530

    update

---
+++ Internal_Assignment/6_program.py
@@ -0,0 +1,5 @@
+def rotate_by_one(arr):
+    return [arr[-1]] + arr[:-1]
+
+arr = [1, 2, 3, 4, 5]
+print(rotate_by_one(arr))
---
+++ Internal_Assignment/7_program.py
@@ -0,0 +1,9 @@
+def max_subarray_sum(arr):
+    max_sum = curr_sum = arr[0]
+    for num in arr[1:]:
+        curr_sum = max(num, curr_sum + num)
+        max_sum = max(max_sum, curr_sum)
+    return max_sum
+
+arr = [2, 3, -8, 7, -1, 2, 3]
+print(max_subarray_sum(arr))
